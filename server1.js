b4a432182b515a802f393fac2800b1181ea57d15
Chicagorp <83872579+Chicagorp@users.noreply.github.com>
2021-06-07 22:27:24 -0300

    make it better

--- server1.js
+++ server1.js
@@ -10,6 +10,7 @@ client.on("ready", () => {
 
    client.user.setPresence( {
        activity: {
+           setImage: "https://cdn.discordapp.com/attachments/840878207724027914/850750397613998100/chicago_rp_no_fondo-2.png",
            name: `+ayuda | 24/7 Moderando Chicago MineCraft`,
            type: "PLAYING"
        },
@@ -26,20 +27,22 @@ client.on('message', async message => {
   const args = message.content.slice(prefix.length).trim().split(/ +/g);
 
   if(message.content.startsWith(prefix + 'ayuda')) {
-    let usuario = message.guild.roles.cache.find((r) => r.id === "828561373994287115");
+    let usuario = message.guild.roles.cache.find((r) => r.id === "850867693342425098");
   
     if (message.member.roles.cache.has(usuario.id)) {
   
       let helpembeduser = new Discord.MessageEmbed()
-      .setTitle('**Chicago Roleplay | Comandos**')
+      .setTitle('**Chicago Minecraft | Comandos**')
       .setDescription('Prefix: "+", Recuerda utilizar estos comando en el canal correcto')
-      .addField('**__Generales__**', "`sugerencia`, `ip`, `reportar`, `normativas`, `dado`, `pregunta`", true)
+      .addField('**__Generales__**', "`sugerencia`, `ip`, `reportar`, `dado`, `pregunta`", true)
       .setColor("RED")
       message.channel.send(helpembeduser);
       }
     }
-// para poner sugerencias
-  if (message.content.startsWith(prefix + 'sugerencia')) {
+
+/////////////////////////////////////////////////  ||  Sug/bugs  ||  /////////////////////////////////////////////////
+
+if (message.content.startsWith(prefix + 'sugerencia')) {
     if (message.channel.id !== "850961616517595177") 
       return message.channel.send(`**Este no es el canal correcto para utilizar este comando.**`);
     message.delete().catch(O_o => {});
@@ -51,7 +54,7 @@ client.on('message', async message => {
       .setAuthor(
         "👍 " +
          message.member.user.username +
-          " ha mandado una sugerencia" +
+          " ha enivado una sugerencia" +
           " ⭐"
       )
       .setFooter(client.user.username, client.user.displayAvatarURL)
@@ -63,7 +66,7 @@ client.on('message', async message => {
   }
   // para poner sugerencias staff
   if (message.content.startsWith(prefix + 'stsugerencia')) {
-    if (message.channel.id !== "850948974553727066") 
+    if (message.channel.id !== "851471916568543292") 
       return message.channel.send(`**Este no es el canal correcto para utilizar este comando.**`);
     message.delete().catch(O_o => {});
     let suggestion = args.slice(1).join(" ");
@@ -74,7 +77,7 @@ client.on('message', async message => {
       .setAuthor(
         "👍 " +
          message.member.user.username +
-          " ha mandado una sugerencia" +
+          " ha enivado una sugerencia" +
           " ⭐"
       )
       .setFooter(client.user.username, client.user.displayAvatarURL)
@@ -86,7 +89,7 @@ client.on('message', async message => {
   }
    // para poner reportar bugs
   if (message.content.startsWith(prefix + 'bug')) {
-    if (message.channel.id !== "828561411441033216") 
+    if (message.channel.id !== "851626650336755744") 
       return message.channel.send(`**Este no es el canal correcto para utilizar este comando.**`);
     message.delete().catch(O_o => {});
     let suggestion = args.slice(1).join(" ");
@@ -97,7 +100,7 @@ client.on('message', async message => {
       .setAuthor(
         "👍 " +
          message.member.user.username +
-          " ha mandado un reporte de un bug" +
+          " ha enviado un reporte de un bug" +
           " ⭐"
       )
       .setFooter(client.user.username, client.user.displayAvatarURL)
@@ -107,10 +110,12 @@ client.on('message', async message => {
       .setColor("BLUE");
       message.channel.send(suggestembed).then(m =>  m.react("✅") + m.react("❌"))
   }
-// para poner una encuesta 
+
+  /////////////////////////////////////////////////  ||  Anuncio/Encuesta  ||  /////////////////////////////////////////////////
+
   if(message.content.startsWith(prefix + 'encuesta')) {
-    if (!message.member.hasPermission("ADMINISTRADOR"))
-      return message.channel.send(`**Tu no puedes utilizar este comando.**`);
+    if (!message.member.hasPermission("ADMIN"))
+      return message.channel.send(`**No tiene los permisos necesarios para utilizar este comando.**`);
     message.delete().catch(O_o => {});
 
     let reporte = args.slice(1).join(" ");
@@ -129,8 +134,8 @@ client.on('message', async message => {
   }
 //anuncio
   if (message.content.startsWith(prefix + "anuncio")) {
-    if (!message.member.hasPermission("ADMINISTRATOR"))
-      return message.channel.send(`**Tu no puedes utilizar este comando.**`);
+    if (!message.member.hasPermission("ADMIN"))
+      return message.channel.send(`**No tiene los permisos necesarios para utilizar este comando.**`);
 
     const sayMessage = args.slice(1).join(" ");
 
@@ -139,17 +144,19 @@ client.on('message', async message => {
     message.channel.send(sayMessage);
   }
 
+/////////////////////////////////////////////////  ||  Verificarse  ||  /////////////////////////////////////////////////
+
   if (message.content.startsWith(prefix + "verificar")) {
-   if (message.channel.id !== "840888177857396746") 
+   if (message.channel.id !== "851472456752955433") 
       return message.channel.send(`**Este no es el canal correcto para utilizar este comando.**`);
       message.channel.send("[Chicago:RP] Estoy revisando tus datos, aguarda un momento...")
       .then(async m => {
         setTimeout(() => {
-          m.edit("[Chicago:RP] ¡Verificando que no eres un robot!");
+          m.edit("[CO:MC] ¡Verificando que no eres un robot!");
         }, 5000);
 
         setTimeout(() => {
-          m.edit("[Chicago:RP] Verificando que no seas una multicuenta...");
+          m.edit("[CO:MC] Verificando que no seas una multicuenta...");
             }, 8000);
         setTimeout(() => {
           const embed = new Discord.MessageEmbed()
@@ -160,75 +167,41 @@ client.on('message', async message => {
         }, 12000); 
 
         setTimeout(() => {
-          message.member.roles.add("828561373994287115");   //poner la id del rol a poner y abajo a quitar
-          message.member.roles.remove("823996149269921812") 
+          message.member.roles.add("850867693342425098");   //poner la id del rol a poner y abajo a quitar
+          message.member.roles.remove("850138042870005801") 
         }, 14000);
       m.delete(20000);
   });
     message.delete().catch(O_o => {});
 }
 
+/////////////////////////////////////////////////  ||  Reportess  ||  /////////////////////////////////////////////////
+
 if(message.content.startsWith(prefix + 'reportar')) {
   let reporte = new Discord.MessageEmbed()
-      .setTitle('**¿Cómo hago para reportar a un usuario/staff?**')
-      .setDescription('1 - Accede al foro y regístrate, link: http://humanprojectrp.rf.gd/\n\n2 - Una vez registrado ve a la sección de reportes\nlink(Usuarios): http://humanprojectrp.rf.gd/forumdisplay.php?fid=8\nlink(Staff): http://humanprojectrp.rf.gd/forumdisplay.php?fid=80\n\n3 - Ve al post de formato y copíalo\nlink(Usuarios): http://humanprojectrp.rf.gd/showthread.php?tid=3\nlink(Staff): http://humanprojectrp.rf.gd/showthread.php?tid=109\n\n4 - Una vez copiado el formato regresa a la sección de reportes y dale click a Nuevo Tema y pega el formato\n\n5 - Una vez pegado el formato en el tema, rellena con los datos correspondientes y dale a Publicar Tema\n\nY listo, tu reporte lo tomará un staff y se analizará, un saludo!\n\n')
+      .setTitle('**¿Cómo hago para reportar a un usuario?**')
+      .setDescription('1 - Dirigete a el correspondiente canal ( #reportes )\n\n2 - Una vez en el canal ve a los mensajes fijados y ve el formato y copíalo\n\n4 - Una vez copiado el formato regresa al canal y pega el formato\n\n5 - Una vez pegado el formato, rellena con los datos correspondientes y envialo.\n\nY listo, tu reporte lo tomará un staff y se analizará, un saludo!\n\n')
       .setTimestamp()
       .setColor("BLUE")
   message.channel.send(reporte);
 }
 
-if(message.content.startsWith(prefix + 'musica')) {
-  let musica = new Discord.MessageEmbed()
-    .setTitle('**¿Cómo poner música en el juego?**')
-    .setDescription('1- Lo primero es estar en un vehículo carro/moto, lo siguiente es poner /parlante.\n\n2- Tienes que entrar a uno de los siguientes links: [https://ytmp3.cc/en13/ o Cualquier Convertidor de YT a mp3]\n\n3- Lo siguiente que tienes que hacer es buscar y copiar el link de descarga de la canción que quieras y prefieras.\n\n4- Como último paso tienes que pegar el URL del video en el converter y darle convertir (converter) y cuando se termine el proceso das click dercho en el boton DESCARGAR o (DOWNLOAD) y das copiar vinculo,una vez copiado el vinculo vas al parlante y pegas ese vinculo, y das crear parlante.\n\n5- Y así, de fácil es poner música si tienes dudas pregúntanos y con gusto te ayudaremos en lo que este a nuestro alcance.\n\n_Un saludo, equipo Administrativo de Chicago Roleplay!_')
-    .setTimestamp()
-    .setColor("BLUE")
-  message.channel.send(musica);
-}
-
-if(message.content.startsWith(prefix + 'foro')) {
-  const foro = new Discord.MessageEmbed()
-      .setColor("BLUE")
-      .setDescription(` **__El Foro es el siguente__**: http://humanprojectrp.rf.gd/`);
-  message.channel.send({ embed: foro });
-}
-
-
-
-if(message.content.startsWith(prefix + 'normativas')) {
-    const normas = new Discord.MessageEmbed()
-        .setColor("BLUE")
-        .setDescription(`Las normativas se encuentran en: \n http://humanprojectrp.rf.gd/ \n **No olvides leerlas**`); // Link de las normas del foro
-    message.channel.send({ embed: normas });
-}
-
+/////////////////////////////////////////////////       ||  IP  ||      /////////////////////////////////////////////////
 if(message.content.startsWith(prefix + 'ip')) {
     const ip = new Discord.MessageEmbed()
         .setColor("BLUE")
-        .setDescription(`La IP del Servidor es la siguente, si no te funciona busca el servidor en el buscador de MTA como "Chicago Roleplay" \n IP: mtasa://135.148.59.55:22523`);
+        .setDescription(`La IP del Servidor es la siguente\n IP: chicagorpmc.aternos.me`);
     message.channel.send({ embed: ip });
 }
 
 
 
-if(message.content.startsWith(prefix + 'ds')) {
-  const ts3 = new Discord.MessageEmbed()
-      .setColor("BLUE")
-      .setDescription(`**Para invitar a alguien al Discord usa la siguiente invitacion** \n __https://discord.gg/kuFfxU5YnB__`);
-  message.channel.send({ embed: ts3 });
-}
 
-
-if(message.content.startsWith(prefix + 'fichaspj')) {
-  const fichas = new Discord.MessageEmbed()
-      .setColor("BLACK")
-      .setDescription(`http://humanprojectrp.rf.gd/forumdisplay.php?fid=48`);
-  message.channel.send({ embed: fichas });
-}
 
 });
+ /////////////////////////////////////////////////  ||  Embeds  ||  /////////////////////////////////////////////////
 
-client.on('message', message =>{
+ client.on('message', message =>{
   if (!message.content.startsWith(prefix)) return; 
   if (message.author.bot) return;
 
@@ -239,14 +212,16 @@ if(message.content.startsWith(prefix + 'avatar')) {
   const avatar = new Discord.MessageEmbed()
     .setTitle('¡Esta es tu foto de perfil!')
     .setColor(0x5E9DE4)
-    .setAuthor(message.author.username, message.author.avatarURL())
+    .setAuthor(message.author.username, client.user.avatarURL())
     .setImage(message.author.avatarURL())
-    .setFooter('Avatar de: '+message.member.displayName, client.user.avatarURL())
+    .setFooter('Avatar de: '+message.member.displayName, message.author.avatarURL())
     .setTimestamp();
   message.channel.send({ embed: avatar});
 }
 });
 
+
+ /////////////////////////////////////////////////  || MiniJuegos ||   /////////////////////////////////////////////////
 
 client.on("message" , msg => {
   var Mensajes = ["1", "2", "3", "4", "5", "6"];
@@ -264,101 +239,84 @@ client.on("message" , msg => {
     }
 });
 
-                  //        Sanciones         //
+ /////////////////////////////////////////////////  || Sanciones ||  /////////////////////////////////////////////////
 
-                        //    kick      //
+ /////////////////////////////////////////////////     || kick ||     /////////////////////////////////////////////////
 
 client.on('message', message => {
-  // Ignore messages that aren't from a guild
+
   if (!message.guild) return;
 
-  // If the message content starts with "!kick"
+
   if (message.content.startsWith('+kick')) {
-    // Assuming we mention someone in the message, this will return the user
-    // Read more about mentions over at https://discord.js.org/#/docs/main/master/class/MessageMentions
+
     const user = message.mentions.users.first();
-    // If we have a user mentioned
+
     if (user) {
-      // Now we get the member from the user
+
       const member = message.guild.member(user);
-      // If the member is in the guild
+
       if (member) {
-        /**
-         * Kick the member
-         * Make sure you run this on a member, not a user!
-         * There are big differences between a user and a member
-         */
+
         member
           .kick('Optional reason that will display in the audit logs')
           .then(() => {
-            // We let the message author know we were able to kick the person
+
             message.reply(`Has kickeado con exito a ${user.tag}`);
           })
           .catch(err => {
-            // An error happened
-            // This is generally due to the bot not being able to kick the member,
-            // either due to missing permissions or role hierarchy
+
             message.reply('No pude patear al miembro');
-            // Log the error
+
             console.error(err);
           });
       } else {
-        // The mentioned user isn't in this guild
+
         message.reply("Ese usuario no está en este servidor!");
       }
-      // Otherwise, if no user was mentioned
+
     } else {
       message.reply("¡No mencionaste al usuario para kickear!");
     }
   }
 });
 
-                                //    ban     //
+/////////////////////////////////////////////////     || ban ||     /////////////////////////////////////////////////
 
 client.on('message', message => {
-  // Ignore messages that aren't from a guild
+
   if (!message.guild) return;
 
-  // if the message content starts with "!ban"
+
   if (message.content.startsWith('+ban')) {
-    // Assuming we mention someone in the message, this will return the user
-    // Read more about mentions over at https://discord.js.org/#/docs/main/master/class/MessageMentions
+
     const user = message.mentions.users.first();
-    // If we have a user mentioned
+
     if (user) {
-      // Now we get the member from the user
+
       const member = message.guild.member(user);
-      // If the member is in the guild
+
       if (member) {
-        /**
-         * Ban the member
-         * Make sure you run this on a member, not a user!
-         * There are big differences between a user and a member
-         * Read more about what ban options there are over at
-         * https://discord.js.org/#/docs/main/master/class/GuildMember?scrollTo=ban
-         */
         member
           .ban({
             reason: '¡Eran malos!',
           })
           .then(() => {
-            // We let the message author know we were able to ban the person
+
             message.reply(`has baneado con éxito a  ${user.tag}`);
           })
           .catch(err => {
-            // An error happened
-            // This is generally due to the bot not being able to ban the member,
-            // either due to missing permissions or role hierarchy
+
             message.reply('No pude prohibir al miembro');
-            // Log the error
+
             console.error(err);
           });
       } else {
-        // The mentioned user isn't in this guild
+
         message.reply("¡Ese usuario no está en este servidor!");
       }
     } else {
-      // Otherwise, if no user was mentioned
+
       message.reply("¡No mencionaste al usuario para banear!");
     }
   }
